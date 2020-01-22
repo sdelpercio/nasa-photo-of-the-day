2560e8fb21d62b525ee811b4f18e529fc7dd399e
Shawn DelPercio <sdelperc@gmail.com>
2020-01-22 18:30:48 -0500

    images now display, but dimensions are all out of wack

--- src/App.js
+++ src/App.js
@@ -7,29 +7,35 @@ import "./App.css";
 function App() {
   const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //   .get('https://api.nasa.gov/planetary/apod?api_key=i1foIZ6W3bEQFHu7fgFJhu4CHaKrcveJxLWYIXmJ&count=7')
-  //   .then(res => {
-  //     console.log(res);
-  //     setData(res.data);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-  // }, []);
+  useEffect(() => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=i1foIZ6W3bEQFHu7fgFJhu4CHaKrcveJxLWYIXmJ&count=7')
+    .then(res => {
+      console.log(res);
+      setData(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
 
   return (
     <div className="App">
       <HeaderBar />
-      <MasonryLayout columns={3} gap={25}>
+      <MasonryLayout className='MasonryContainer' columns={3} gap={20}>
         {
-          [...Array(12).keys()].map(key => {
+          [...Array(7).keys()].map(key => {
             const height = 200 + Math.ceil(Math.random() * 300);
 
+            if (!data[key]) {
+              return (
+                <div style={{height: `${height}px`}} />
+              )
+            }
             return (
-              <div style={{height: `${height}px`}} />
+              <img className='MasonryImg' style={{height: `${height}px`}} src={data[key].url} />
             )
+            
           })
         }
       </MasonryLayout>
