61e55b03d749fa5af067b966cbb9c12c1aee8de4
Shawn DelPercio <sdelperc@gmail.com>
2020-01-22 15:53:31 -0500

    add nasa logo to assets folder; add HeaderBar jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
+import HeaderBar from './components/HeaderBar/HeaderBar.js';
+// import MasonryLayout from './components/MasonryLayout/MasonryLayout.js';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <HeaderBar />
     </div>
   );
 }
--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -1,3 +1,13 @@
 import React from 'react';
+import logo from '../../assets/NASA_Worm_logo.svg';
+
+function HeaderBar() {
+    return (
+        <div className='HeaderBar'>
+            <img className='HeaderImg' src={logo} alt='NASA Logo'/>
+            <h1 className='HeaderText'>NASA Photo of the Day</h1>
+        </div>
+    )
+}
 
 export default HeaderBar;
