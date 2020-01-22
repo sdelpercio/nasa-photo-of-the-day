24d5e5be868a8166db7ebc794405a1a470474ad2
Shawn DelPercio <sdelperc@gmail.com>
2020-01-22 16:30:28 -0500

    add state, use effect for nasa api; add img tag to masonry layout to test api call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import HeaderBar from './components/HeaderBar/HeaderBar.js';
-// import MasonryLayout from './components/MasonryLayout/MasonryLayout.js';
+import MasonryLayout from './components/MasonryLayout/MasonryLayout.js';
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+      console.log(res);
+      setData(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
+
   return (
     <div className="App">
       <HeaderBar />
+      <MasonryLayout data={data}/>
     </div>
   );
 }
